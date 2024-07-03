618acdf069134baae4642017a0e2a0feca01f3a9
Julien Martel <julien@monogram.io>
2024-07-03 14:23:11 -0500

    fixes

--- dev.nix
+++ dev.nix
@@ -5,9 +5,7 @@
   channel = "stable-23.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.nodePackages.firebase-tools
-    # pkgs.jdk17
-    # pkgs.unzip
+    
   ];
   # Sets environment variables in the workspace
   env = {};
@@ -19,11 +17,16 @@
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
-        build-flutter = ''
-          adb -s localhost:5554 wait-for-device
+        npm-install = ''
+          npm i
         '';
       };
-      # To run something each time the workspace is (re)started, use the `onStart` hook
+      # Runs when a workspace restarted
+      onStart = {
+        wait-for-device = ''
+          adb -s localhost:5555 wait-for-device
+        '';
+      };
     };
     # Enable previews and customize configuration
     previews = {
@@ -34,7 +37,7 @@
           manager = "web";
         };
         android = {
-          command = ["npm" "run" "android" "--" "--port" "5554"];
+          command = ["npm" "run" "android" "--" "--port" "5555"];
           manager = "flutter";
         };
       };
--- idx-template.nix
+++ idx-template.nix
@@ -4,7 +4,7 @@
   ];
   bootstrap = ''
     mkdir -p "$WS_NAME"
-    npx -y create-expo-app@latest "$WS_NAME"
+    npx -y create-expo-app@latest "$WS_NAME" --no-install
     mkdir "$WS_NAME/.idx/"
     cp ${./dev.nix} "$WS_NAME/.idx/dev.nix"
     chmod -R +w "$WS_NAME"
