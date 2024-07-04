fcfd407061aa4250234236caeb7d4cd3bb3075c5
Julien Martel <julien@monogram.io>
2024-07-03 23:56:56 -0500

    fixes

--- idx-template.nix
+++ idx-template.nix
@@ -11,12 +11,3 @@
     mv "$WS_NAME" "$out"
   '';
 }
-
-#  mkdir -p "$WS_NAME"
-#   npm create -y vite@latest "$WS_NAME" -- --template ${if language == "ts" then "react-ts" else "react"}
-#   mkdir -p "$WS_NAME/.idx/"
-#   cp -rf ${./icon.png} "$WS_NAME/.idx/icon.png"
-#   cp -rf ${./dev.nix} "$WS_NAME/.idx/dev.nix"
-#   chmod -R +w "$WS_NAME"
-#   mv "$WS_NAME" "$out"
-#   cd "$out"; npm install --package-lock-only --ignore-scripts
