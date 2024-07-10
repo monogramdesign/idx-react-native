c1232cc5c4241c21b62d0e6d3e09a10218bf8f3a
Julien Martel <julien@monogram.io>
2024-07-09 21:11:29 -0500

    fixes

--- idx-template.nix
+++ idx-template.nix
@@ -17,6 +17,7 @@
     }
     mkdir "$WS_NAME/.idx/"
     packageManager=${packageManager} j2 ${./devNix.j2} -o "$WS_NAME/.idx/dev.nix"
+    packageManager=${packageManager} j2 ${./README.j2} -o "$WS_NAME/README.md"
     chmod -R +w "$WS_NAME"
     mv "$WS_NAME" "$out"
   '';
